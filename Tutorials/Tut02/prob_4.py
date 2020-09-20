98c1cf81592117f2a4188128b07fed270ab68caf
trantu147 <trantu1472@gmail.com>
2020-09-20 11:50:47 +0700

    Tut2_hw

---
+++ Tutorials/Tut02/prob_4.py
@@ -0,0 +1,4 @@
+total_Sale = float(input("Please enter the projected amount of total sales: $"))
+profit = total_Sale * 0.23
+print("The profit will be made: $",format(profit, ',.2f'))
+print("Hello")
---
+++ Tutorials/Tut02/prob_6.py
@@ -0,0 +1,7 @@
+v = 70
+d1 = v*6
+d2 = v*10
+d3 = v*15
+print("The distance the car will travel in 6 hours: ",d1, " miles.")
+print("The distance the car will travel in 10 hours: ",d2, " miles.")
+print("The distance the car will travel in 15 hours: ", d3, " miles.")
